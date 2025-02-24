402df2c230f07d4470a084d68e84bfd705f2bd7d
Donskov <donskov_a@icloud.com>
2025-02-24 14:20:04 +0500

    request logging

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -5,17 +5,33 @@ import (
 	"net/http"
 )
 
+// TODO:Проверить с Артуром
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func NewLoggingResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
+	return &loggingResponseWriter{w, http.StatusOK}
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
+
 // TODO: off default logger
 func RequestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			next.ServeHTTP(w, r)
+			lrw := NewLoggingResponseWriter(w)
+			next.ServeHTTP(lrw, r)
 			requestID, _ := r.Context().Value(RequestIDCtxKey).(RequestID)
 			logger.Info(
 				"http request",
 				slog.String("method", r.Method),
 				slog.String("path", r.URL.Path),
-				//slog.Int64("status", int64(r.Response.StatusCode)),
+				slog.Int64("status", int64(lrw.statusCode)),
 				slog.String("request_id", string(requestID)),
 			)
 		})
