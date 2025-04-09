68c8ca3d73e4e24e56735bc9fb892c863e79cccd
Alex Donskov <donskov_a@icloud.com>
2025-04-09 11:26:34 +0500

    adding mock tests ingredients_test.go

--- internal/processors/ingredients_test.go
+++ internal/processors/ingredients_test.go
@@ -94,7 +94,146 @@ func (s *IngredientsTestSuite) TestCreateIngredientSuccess() {
 }
 
 func (s *IngredientsTestSuite) TestCreateIngredientError() {
+	dbError := errors.New("db error")
+	expectedIngredient := &domain.Ingredient{
+		ID:       1,
+		UUID:     "1",
+		Name:     "FirstIngredient",
+		Quantity: 16,
+	}
+	expectedError := fmt.Errorf("unable to create ingredient: %s, error: %w",
+		expectedIngredient.Name, dbError)
 
+	gomock.InOrder(
+		s.repository.
+			EXPECT().
+			CreateIngredient(gomock.Any(), gomock.Any()).
+			Return(nil, dbError),
+		s.logger.
+			EXPECT().
+			Error(gomock.Any(), gomock.Any()),
+	)
+	actualIngredient, err := s.processor.CreateIngredient(s.ctx, expectedIngredient)
+	require.Nil(s.T(), actualIngredient)
+	require.EqualError(s.T(), err, expectedError.Error())
+}
+
+func (s *IngredientsTestSuite) TestIngredientByUUIDSuccess() {
+	expectedIngredient := &domain.Ingredient{
+		ID:       1,
+		UUID:     "1",
+		Name:     "FirstIngredient",
+		Quantity: 16,
+	}
+
+	gomock.InOrder(
+		s.repository.
+			EXPECT().
+			IngredientByUUID(gomock.Any(), gomock.Any()).
+			Return(expectedIngredient, nil),
+	)
+
+	actualIngredient, err := s.processor.GetIngredientByUUID(s.ctx, "1")
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), expectedIngredient, actualIngredient)
+}
+
+func (s *IngredientsTestSuite) TestIngredientByUUIDError() {
+	dbError := errors.New("db error")
+	expectedError := fmt.Errorf("unable to get ingredient by uuid: %s, error: %w",
+		"1", dbError)
+
+	gomock.InOrder(
+		s.repository.
+			EXPECT().
+			IngredientByUUID(gomock.Any(), gomock.Any()).
+			Return(nil, dbError),
+		s.logger.
+			EXPECT().
+			Error(gomock.Any(), gomock.Any()),
+	)
+
+	actualIngredient, err := s.processor.GetIngredientByUUID(s.ctx, "1")
+
+	require.Nil(s.T(), actualIngredient)
+	require.EqualError(s.T(), err, expectedError.Error())
+}
+
+func (s *IngredientsTestSuite) TestDeleteIngredientByUUIDSuccess() {
+	gomock.InOrder(
+		s.repository.
+			EXPECT().
+			DeleteIngredientByUUID(gomock.Any(), gomock.Any()).
+			Return(nil))
+
+	err := s.processor.DeleteIngredientByUUID(s.ctx, "1")
+	require.NoError(s.T(), err)
+}
+func (s *IngredientsTestSuite) TestDeleteIngredientByUUIDError() {
+	dbError := errors.New("db error")
+	expectedError := fmt.Errorf("unable to delete ingredient by uuid: %s, error: %w",
+		"1", dbError)
+
+	gomock.InOrder(
+		s.repository.
+			EXPECT().
+			DeleteIngredientByUUID(gomock.Any(), gomock.Any()).
+			Return(dbError),
+		s.logger.
+			EXPECT().
+			Error(gomock.Any(), gomock.Any()),
+	)
+
+	err := s.processor.DeleteIngredientByUUID(s.ctx, "1")
+	require.EqualError(s.T(), err, expectedError.Error())
+}
+
+func (s *IngredientsTestSuite) TestUpdateIngredientByUUIDSuccess() {
+	expectedIngredient := &domain.Ingredient{
+		ID:       1,
+		UUID:     "1",
+		Name:     "FirstIngredient",
+		Quantity: 16,
+	}
+
+	gomock.InOrder(
+		s.repository.
+			EXPECT().
+			UpdateIngredientByUUID(gomock.Any(), gomock.Any()).
+			Return(expectedIngredient, nil),
+	)
+
+	actualIngredient, err := s.processor.UpdateIngredientByUUID(s.ctx, expectedIngredient)
+
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), expectedIngredient, actualIngredient)
+}
+
+func (s *IngredientsTestSuite) TestUpdateIngredientByUUIDError() {
+	dbError := errors.New("db error")
+	expectedIngredient := &domain.Ingredient{
+		ID:       1,
+		UUID:     "1",
+		Name:     "FirstIngredient",
+		Quantity: 16,
+	}
+	expectedError := fmt.Errorf("unable to update ingredient by uuid: %s, error: %w",
+		expectedIngredient.UUID, dbError)
+
+	gomock.InOrder(
+		s.repository.
+			EXPECT().
+			UpdateIngredientByUUID(gomock.Any(), gomock.Any()).
+			Return(nil, dbError),
+		s.logger.
+			EXPECT().
+			Error(gomock.Any(), gomock.Any()),
+	)
+
+	actualIngredient, err := s.processor.UpdateIngredientByUUID(s.ctx, expectedIngredient)
+
+	require.Nil(s.T(), actualIngredient)
+	require.EqualError(s.T(), err, expectedError.Error())
 }
 
 func TestIngredientsTestSuite(t *testing.T) {
